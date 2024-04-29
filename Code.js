2ce28a8fee160204bf54151980ae5ecd22024f38
Caharaa <nutthaphong.tbs@gmail.com>
2024-04-29 11:43:31 +0700

    remove old element befornew element are created

--- Code.js
+++ Code.js
@@ -11,9 +11,15 @@ function get_user(){
   const data = JSON.parse(response.getContentText());
   return data
 }
-
+function generating_id(message){
+  var key = "saleKey";
+  var cipher = Utilities.base64Encode(Utilities.computeHmacSha256Signature(message, key));
+  return cipher;
+}
 function display_in_row(text_array){
   var sheet  = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  sheet.appendRow(text_array)
+  var recordId = generating_id(text_array[2]+text_array[4]);
+  text_array.push("ID : "+recordId);
+  sheet.appendRow(text_array);
 
 }
--- sidebar.js
+++ sidebar.js
@@ -3,40 +3,47 @@ function create_import_butt(stemid) {
   var import_button = document.createElement('button');
   import_button.style.margin = '5px';
   import_button.style.padding = '10px';
-  import_button.id = 'ID: ' + stemid;
+  import_button.id = 'ID: '+stemid;
   import_button.innerHTML = 'Import';
-  import_button.addEventListener('click', extration);
+  import_button.addEventListener('click',extration);
   element.appendChild(import_button);
 }
-function create_user_info(user, stemid) {
+function create_user_info(user,stemid) {
   var element = document.getElementById("Det");
   var userDiv = document.createElement('div'); // Create a new <div> element
-  userDiv.innerHTML = '<p>' + 'Gender: ' + user[0].gender + '<p>' +
-    '<p>' + 'Age: ' + user[0].dob.age + '<p>' +
-    '<p>' + 'Email: ' + user[0].email + '<p>' +
-    '<p>' + 'City: ' + user[0].location.city + '<p>' +
-    '<p>' + 'Phone: ' + user[0].phone + '<p>';
+  userDiv.innerHTML = '<p>' +'Gender: '+ user[0].gender + '<p>' +
+        '<p>'+'Age: ' + user[0].dob.age + '<p>' +
+        '<p>'+'Email: ' + user[0].email + '<p>' +
+        '<p>'+'City: ' + user[0].location.city + '<p>' +
+        '<p>'+'Phone: ' + user[0].phone + '<p>';
   userDiv.style.backgroundColor = 'lightblue';
   userDiv.style.padding = '5px';
   userDiv.style.margin = '5px';
   userDiv.style.width = '200px';
   userDiv.style.height = '200px';
-  userDiv.id = 'ID: ' + stemid
+  userDiv.id = 'ID: '+stemid
   element.appendChild(userDiv);
 }
 function replace_info(user) {
-  const stemid = new Date().getTime();
-  create_user_info(user.results, stemid);
-  create_import_butt(stemid);
+  var parent = document.getElementById('Det')
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
 }
-function extration(event) {
+  var object = 10;
+  for(let i=0;i < 10;i++){
+  const stemid = new Date().getTime();
+  create_user_info(user.results,stemid);
+  create_import_butt(stemid);
+  }
+}
+
+function extration(event){
   var id = this.id;
   var extrac_element = document.getElementById(id);
   var child = extrac_element.childNodes;
   var text_array = [child[0].innerHTML, child[2].innerHTML, child[4].innerHTML, child[6].innerHTML, child[8].innerHTML];
   google.script.run.display_in_row(text_array);
 }
-
 function send_request() {
   google.script.run.withSuccessHandler(replace_info).get_user();
 }
