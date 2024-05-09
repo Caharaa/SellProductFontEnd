d94b2f753b2960318977dd6330ee72bf7f3d4292
Caharaa <nutthaphong.tbs@gmail.com>
2024-05-09 14:30:08 +0700

    extraction all add more input(version1)

--- Code.js
+++ Code.js
@@ -6,15 +6,21 @@ function show_admin_sidebar() {
   ui.showSidebar(html);
 }
 
-function get_user(keyword) {
+function get_location(searchBody) {
   var url = 'https://cloud.gmapsextractor.com/api/v1/search';
-  var token = 'gUGwrX9tTYBL4STjvdlAvYZQ8dzzz9M9cPf14PiPfIhRCDsv'; 
+  var token = 'gUGwrX9tTYBL4STjvdlAvYZQ8dzzz9M9cPf14PiPfIhRCDsv'; // Replace 'YOUR_TOKEN' with your actual token
+  var keyword = searchBody.query;
+  var page = searchBody.page;
+  var ll = searchBody.ll;
+  var hl = searchBody.hl;
+  var gl = searchBody.gl;
+
   var payload = {
     q: keyword,
-    page: 1,
-    ll: '@23.0875893,112.3725638,11z',
-    hl: 'th',
-    gl: 'th'
+    page: page,
+    ll:ll,
+    hl: hl,
+    gl: gl,
   };
 
   var options = {
@@ -30,8 +36,45 @@ function get_user(keyword) {
   var responseData = JSON.parse(response.getContentText());
   return responseData
 }
-
 function display_in_row(text_array) {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  sheet.appendRow(text_array);
+  sheet.appendRow(text_array)
+}
+function display_all_in_row(text_array) {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var row = 2;
+  var i = 0;
+  while (true) {
+    if (sheet.getRange(row, 1).getValue() === "") {
+      sheet.getRange(row, 1).setValue("name: "+text_array[i].name);
+      sheet.getRange(row, 2).setValue("fulladdress: "+text_array[i].full_address)
+      sheet.getRange(row, 3).setValue("street: "+text_array[i].street)
+      sheet.getRange(row, 4).setValue("municioality: "+text_array[i].municipality)
+      sheet.getRange(row, 5).setValue("categories: "+text_array[i].categories)
+      sheet.getRange(row, 6).setValue("phone: "+text_array[i].phone)
+      sheet.getRange(row, 7).setValue("phones: "+text_array[i].phones)
+      sheet.getRange(row, 8).setValue("claimed: "+text_array[i].claimed)
+      sheet.getRange(row, 9).setValue("reviewcount: "+text_array[i].review_count)
+      sheet.getRange(row, 10).setValue("averagerating: "+text_array[i].average_rating)
+      sheet.getRange(row, 11).setValue(text_array[i].review_url)
+      sheet.getRange(row, 12).setValue("latitude: "+text_array[i].latitude)
+      sheet.getRange(row, 13).setValue("longitude: "+text_array[i].longitude)
+      sheet.getRange(row, 14).setValue(text_array[i].website)
+      sheet.getRange(row, 15).setValue("domain: "+text_array[i].domain)
+      sheet.getRange(row, 16).setValue("openinghours: "+text_array[i].opening_hours)
+      sheet.getRange(row, 17).setValue("featuredimage: "+text_array[i].featured_image)
+      sheet.getRange(row, 18).setValue(text_array[i].google_maps_url)
+      sheet.getRange(row, 19).setValue(text_array[i].google_knowledge_url)
+      sheet.getRange(row, 20).setValue("cid: "+text_array[i].cid)
+      sheet.getRange(row, 21).setValue("kgmid: "+text_array[i].kgmid)
+      sheet.getRange(row,22).setValue("placeid: "+text_array[i].place_id)
+      i++;
+      row++;
+    } else {
+      row++;
+    }
+    if (i === text_array.length) {
+      break;
+    }
+  }
 }
--- sidebar.js
+++ sidebar.js
@@ -1,92 +1,95 @@
-function create_import_butt(stampid) {
-  var element = document.getElementById("Det");
-  var import_button = document.createElement('button');
-  import_button.style.margin = '5px';
-  import_button.style.padding = '10px';
-  import_button.id = 'ID: ' + stampid;
-  import_button.innerHTML = 'Import';
-  import_button.addEventListener('click', extration);
-  element.appendChild(import_button);
+var stage = {};
+function clear_object(){
+  stage = null;
+  stage = {}
 }
-function create_user_info(place, stampid) {
-  var element = document.getElementById("Det");
-  var userDiv = document.createElement('div'); // Create a new <div> element
-  userDiv.innerHTML = '<p>' + 'name: ' + place.name + '<p>' +
-    '<p>' + 'full_address: ' + place.full_address + '<p>' +
-    '<p>' + 'street: ' + place.street + '<p>' +
-    '<p>' + 'municipality: ' + place.municipality + '<p>' +
-    '<p>' + 'categories: ' + place.categories + '<p>' +
-    '<p>' + 'claimed: ' + place.claimed + '<p>' +
-    '<p>' + 'review_count: ' + place.review_count + '<p>' +
-    '<p>' + 'average_rating: ' + place.average_rating + '<p>' +
-    `<p>review_url: <a href=${place.review_url} target=_blank>visite review</a></p>` +
-    '<p>' + 'latitude: ' + place.latitude + '<p>' +
-    '<p>' + 'longitude: ' + place.longitude + '<p>' +
-    '<p>' + 'phones: ' + place.phones + '<p>' +
-    '<p>' + 'domain: ' + place.domain + '<p>' +
-    `<p> website: <a href= https:/${place.website} target=_blank>visite website</a></p>` +
-    `<p>featured_image: <br> <img src=${place.featured_image}><p>` +
-    `<p>google_map_url: <a href=${place.google_map_url} target=_blank>visite map</a></p>` +
-    `<p>google_knowledge_url: <a href=${place.google_knowledge_url} target=_blank>visite knowledge</a><p>` +
-    '<p>' + 'cid: ' + place.cid + '<p>' +
-    '<p>' + 'kgmid: ' + place.kgmid + '<p>' +
-    '<p>' + 'place_id: ' + place.place_id + '<p>';
-  userDiv.style.backgroundColor = 'lightblue';
-  userDiv.style.padding = '5px';
-  userDiv.style.margin = '5px';
-  userDiv.id = 'ID: ' + stampid
-  element.appendChild(userDiv);
+function create_import_butt(stampid) {
+var element = document.getElementById("Det");
+var import_button = document.createElement('button');
+import_button.style.margin = '5px';
+import_button.style.padding = '10px';
+import_button.id = stampid;
+import_button.innerHTML = 'Import';
+import_button.addEventListener('click',extraction);
+element.appendChild(import_button);
+}
+function create_user_info(place,stampid) {
+var element = document.getElementById("Det");
+var userDiv = document.createElement('div'); // Create a new <div> element
+userDiv.innerHTML = '<p>' +'name: '+ place.name + '<p>' +
+      '<p>' +'full_address: '+ place.full_address + '<p>' +
+      '<p>' +'street: '+ place.street + '<p>' +
+      '<p>'+'municipality: ' + place.municipality + '<p>' +
+      '<p>'+'categories: ' + place.categories + '<p>' +
+      '<p>'+'claimed: ' + place.claimed + '<p>' +
+      '<p>'+'review_count: ' + place.review_count + '<p>' +
+      '<p>'+'average_rating: ' + place.average_rating + '<p>' +
+      `<p>review_url: <a href=${place.review_url} target=_blank>visite review</a></p>`+
+      '<p>'+'latitude: ' + place.latitude + '<p>' +
+      '<p>'+'longitude: ' + place.longitude + '<p>' +
+      '<p>'+'phones: ' + place.phones + '<p>' +
+      '<p>'+'domain: ' + place.domain + '<p>' +
+      `<p> website: <a href= https:/${place.website} target=_blank>visite website</a></p>`+
+      `<p>featured_image: <br> <img src=${place.featured_image}><p>`+
+      `<p>google_map_url: <a href=${place.google_maps_url} target=_blank>visite map</a></p>` +
+      `<p>google_knowledge_url: <a href=${place.google_knowledge_url} target=_blank>visite knowledge</a><p>`+
+      '<p>'+'cid: ' + place.cid + '<p>'+
+      '<p>'+'kgmid: ' + place.kgmid + '<p>'+
+      '<p>'+'place_id: ' + place.place_id + '<p>';
+userDiv.style.backgroundColor = 'lightblue';
+userDiv.style.padding = '5px';
+userDiv.style.margin = '5px';
+userDiv.id = stampid;
+element.appendChild(userDiv);
 }
 function replace_info(places) {
-  var parent = document.getElementById('Det');
-
-  if (places.data.length === 0) {
-    console.log("not found");
-    parent.innerHTML = '<p>' + 'NOT FOUND' + '</p>';
-  } else {
-    while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-    }
-    for (let i = 0; i < places.total; i++) {
-      if (!(places.data[i].website === undefined)) {
-        create_user_info(places.data[i], i);
-        create_import_butt(i);
-      }
+var parent = document.getElementById('Det');
+if (places.data.length === 0) {
+  console.log("not found");
+  parent.innerHTML = '<p>' + 'NOT FOUND' + '</p>';
+} else {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+  for (let i = 0; i < places.total; i++) {
+    if(!(places.data[i].website === undefined)){
+      store_data(places.data[i],i);
+      create_user_info(places.data[i], i);
+      create_import_butt(i);
     }
   }
 }
-
-function extration(event) {
-  var id = this.id;
-  var extrac_element = document.getElementById(id);
-  var child = extrac_element.childNodes;
-  var text_array = []
-  for (let i = 0; i < child.length; i++) {
-    if (child[i].innerHTML !== "") {
-      text_array.push(child[i].innerHTML);
-    }
-  }
-  // upon to the googlemap extraction json
-  var url_web = text_array[13];
-  var url_review = text_array[8];
-  var url_map = text_array[15];
-  var url_know = text_array[16];
-  text_array[13] = add_hyper_link(url_web);
-  text_array[8] = add_hyper_link(url_review);
-  text_array[15] = add_hyper_link(url_map);
-  text_array[16] = add_hyper_link(url_know);
-  google.script.run.display_in_row(text_array);
 }
-function add_hyper_link(url) {
-  var tempElement = document.createElement('div');
-  tempElement.innerHTML = `${url}`
-  var linkElement = tempElement.querySelector('a');
-  var url = linkElement.getAttribute('href');
-  return url;
+function store_data(place,stampid){
+stage[stampid] = place;
+}
+function extraction_all(){
+var textes_array = [];
+for(let i in stage){
+  textes_array.push(stage[i]);
+}
+google.script.run.display_all_in_row(textes_array);
+clear_object();
 }
 
-function send_request() {
-  var keyword = document.getElementById('url').value;
-  google.script.run.withSuccessHandler(replace_info).test_data_json();
-  //google.script.run.withSuccessHandler(replace_info).get_user(keyword);
+function extraction(event){
+var stampId = this.id;
+const locationObj = stage[stampId];
+console.log(locationObj);
+var locationArray = [];
+for(let i in locationObj){
+  locationArray.push(locationObj[i]);
+}
+google.script.run.display_in_row(locationArray);
+}
+
+function send_request(event){
+event.preventDefault();
+
+const query = document.getElementById("q").value
+const page = document.getElementById("page").value
+const ll = "@23.0875893,112.3725638,11z";
+const hl = document.getElementById("hl").value
+const gl = document.getElementById("gl").value
+google.script.run.withSuccessHandler(replace_info).get_location({query,page,ll,hl,gl})
 }
